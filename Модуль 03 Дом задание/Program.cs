f492655d8a529d4eb404c5edba9eab28c3b9908c
Alexander Chechetko <ggg.kacheck@gmail.com>
2023-09-18 20:47:10 +0600

    Добавьте файлы проекта.

---
+++ Модуль 03 Дом задание/Program.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Модуль_03_Дом_задание
+{
+
+
+    class Program
+    {
+        static void Zad1()
+        {
+            //  массив целых чисел
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            // вывод массив
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.ReadKey();
+
+        }
+
+        static void Zad2()
+        {
+            Console.Write("Массив состоит и 5 чисел. Введите  Индекс элемента, который нужно удалить");
+            // Создаем массив целых чисел
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+
+            int indexToDelete = Convert.ToInt32(Console.ReadLine());
+            // Индекс элемента, который нужно удалить
+
+
+            // Проверяем, что индекс находится в пределах массива
+            if (indexToDelete >= 0 && indexToDelete < numbers.Length)
+            {
+                // Создаем новый массив с уменьшенным размером
+                int[] newArray = new int[numbers.Length - 1];
+
+                // Копируем элементы до удаляемого элемента
+                for (int i = 0, j = 0; i < numbers.Length; i++)
+                {
+                    if (i != indexToDelete)
+                    {
+                        newArray[j] = numbers[i];
+                        j++;
+                    }
+                }
+
+                // Обновляем исходный массив
+                numbers = newArray;
+
+                Console.WriteLine("Массив после удаления элемента:");
+                foreach (int number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверный индекс для удаления элемента.");
+            }
+        }
+
+        static void Zad3() //Вставить элемент в массив по индексу
+        {
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            // Индекс, по которому нужно вставить элемент.
+            int Vstavit = 2;
+
+            // Элемент, который нужно вставить.
+            int elementVstavit = 10;
+
+            // Проверяем, что индекс находится в допустимых пределах массива.
+            if (Vstavit >= 0 && Vstavit < numbers.Length)
+            {
+                // Присваиваем новое значение по указанному индексу.
+                numbers[Vstavit] = elementVstavit;
+
+                // Выводим результат.
+                foreach (int number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+
+        static void Zad4() //Удалить те элементы массива, которые встречаются в нем ровно два раза
+        {
+
+            int[] numbers = { 1, 2, 2, 3, 4, 4, 5, 6, 6 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (i != j && numbers[i] == numbers[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count != 2)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+        }
+
+        static void Zad5()
+        {
+
+            string input = "Я ел Арбузы и бежал за слоном.";
+
+            // Разбиваем строку на слова по пробелам.
+            string[] words = input.Split(' ');
+
+            // Получаем последнее слово.
+            string Word = words.LastOrDefault();
+
+            if (Word != null)
+            {
+                // Получаем уникальные буквы из последнего слова.
+                char[] uniquelbuk = Word.Distinct().ToArray();
+
+                // Формируем новую строку, исключая слова, содержащие буквы из последнего слова.
+                string result = string.Join(" ", words.Where(word => !uniquelbuk.Any(letter => word.Contains(letter))));
+
+                Console.WriteLine(result);
+            }
+        }
+
+        static void Zad6()
+        {
+
+            int[,] matrix = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+            int numRows = matrix.GetLength(0);
+            int numCols = matrix.GetLength(1);
+
+            // Определяем главную диагональ и обнуляем строки, на пересечении которых с ней стоит четный элемент.
+            for (int i = 0; i < numRows; i++)
+            {
+                if (matrix[i, i] % 2 == 0)
+                {
+                    for (int j = 0; j < numCols; j++)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            // Выводим результат.
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Zad7()
+        {
+
+            Console.WriteLine("Введите текст. Для завершения введите точку.");
+
+            char input;
+            int spaceCount = 0;
+
+            do
+            {
+                input = Console.ReadKey().KeyChar;
+
+                if (input == ' ')
+                {
+                    spaceCount++;
+                }
+
+            } while (input != '.');
+
+            Console.WriteLine("\nКоличество введенных пробелов: " + spaceCount);
+        }
+        static void Zad8()
+        {
+            Console.WriteLine("Введите номер трамвайного билета (6-значное число):");
+            string input = Console.ReadLine();
+
+            if (input.Length == 6 && int.TryParse(input, out int ticket))
+            {
+                int sumFirst = GetSum(ticket / 1000);
+                int sumLast = GetSum(ticket % 1000);
+
+                if (sumFirst == sumLast)
+                {
+                    Console.WriteLine("Этот билет считается счастливым!");
+                }
+                else
+                {
+                    Console.WriteLine("Этот билет не считается счастливым.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите 6-значное число.");
+            }
+        }
+
+        static int GetSum(int number)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            return sum;
+        }
+        static void Zad9()
+        {
+            string word = "класс";
+            char[] wordArray = word.ToCharArray();
+
+            for (int i = 0; i < wordArray.Length - 1; i++)
+            {
+                if (wordArray[i] == wordArray[i + 1])
+                {
+                    wordArray[i] = '1';
+                    wordArray[i + 1] = '1';
+                }
+            }
+
+            string result = new string(wordArray);
+            Console.WriteLine(result);
+
+        }
+        static void Zad10()
+        {
+
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            int digitCount = CountDigits(input);
+
+            Console.WriteLine($"Количество цифр в строке: {digitCount}");
+        }
+
+        static int CountDigits(string input)
+        {
+            int digitCount = 0;
+
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    digitCount++;
+                }
+            }
+
+            return digitCount;
+        }
+        static void Zad11()
+        {
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+
+            bool containsOne = ContainsWord(text, "one");
+
+            if (containsOne)
+            {
+                Console.WriteLine("Слово 'one' найдено в тексте.");
+            }
+            else
+            {
+                Console.WriteLine("Слово 'one' не найдено в тексте.");
+            }
+        }
+
+        static bool ContainsWord(string text, string word)
+        {
+            string[] words = text.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string w in words)
+            {
+                if (string.Equals(w, word, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    
+        static void Zad12()
+        {
+
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            char vstrecha = 'P';
+            int count = 0;
+
+            foreach (char c in input)
+            {
+                if (c == 'P' || c == 'p') // Учитываем как большие, так и маленькие буквы.
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Буква '{vstrecha}' встречается {count} раз.");
+        
+    }
+        static void Zad13()
+        {
+
+            
+                Console.WriteLine("Введите текст:");
+                string input = Console.ReadLine();
+
+                // Создаем массив слов, разбивая строку по пробелам.
+                string[] words = input.Split(' ');
+
+                Console.WriteLine($"Количество слов в строке: {words.Length}");
+                Console.WriteLine("Слова в столбик:");
+
+                // Выводим слова в столбик, перебирая массив слов.
+                foreach (string word in words)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        
+        static void Zad14()
+        {
+
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            // Разбиваем строку на слова, используя пробел в качестве разделителя.
+            string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int count = 0;
+
+            // Перебираем каждое слово и проверяем первый и последний символ.
+            foreach (string word in words)
+            {
+                if (word.Length > 0 && word[0] == word[word.Length - 1])
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Количество слов, у которых первый и последний символы совпадают: {count}");
+        
+    }
+          
+        static void Zad15()
+        {
+
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            // Разбиваем строку на слова, используя пробел в качестве разделителя.
+            string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            char openBracket = '[';
+            char closeBracket = ']';
+
+            foreach (string word in words)
+            {
+                if (word.Length >= 2 && word[0] == word[word.Length - 1] && char.IsLetter(word[0]))
+                {
+                    // Заменяем слово в строке, добавляя квадратные скобки.
+                    input = input.Replace(word, $"{openBracket}{word}{closeBracket}");
+                }
+            }
+
+            Console.WriteLine("Строка с выделенными словами:");
+            Console.WriteLine(input);
+        }
+            static void Main(string[] args)
+        {
+            //Zad1();
+            //Zad2();
+            //Zad3();
+            //Zad4();
+           // Zad5();
+           // Zad6();
+         //   Zad7();
+        //    Zad8();
+        //    Zad9();
+        //    Zad10();
+         //   Zad11();
+            Zad12();
+            Zad13();
+            Zad14();
+            Zad15();
+        }
+
+
+    }
+
+}
---
+++ Практик 03/Program.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практик_03
+{
+    class Program
+    {
+
+        static void Zad1()
+        {
+
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 3, 4, 5, 6, 7 };
+
+            int[] commonElements = GetElements(array1, array2);
+
+            Console.WriteLine("Общие элементы массивов без повторений:");
+            foreach (int element in commonElements)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static int[] GetElements(int[] array1, int[] array2)
+        {
+            // Создаем массив для хранения общих элементов.
+            int[] commonElements = new int[Math.Min(array1.Length, array2.Length)];
+            int count = 0;
+
+            // Перебираем элементы первого массива.
+            foreach (int element1 in array1)
+            {
+                // Проверяем, есть ли такой элемент во втором массиве и в массиве общих элементов.
+                if (Array.Exists(array2, element2 => element2 == element1) && !Array.Exists(commonElements, element => element == element1))
+                {
+                    commonElements[count] = element1;
+                    count++;
+                }
+            }
+
+            // Создаем новый массив с точным размером.
+            int[] result = new int[count];
+            Array.Copy(commonElements, result, count);
+
+            return result;
+
+        }
+
+        static void Zad2()
+        {
+
+            Console.WriteLine("Введите предложение:");
+            string input = Console.ReadLine();
+
+            int wordCount = CountWords(input);
+
+            Console.WriteLine($"Количество слов в предложении: {wordCount}");
+        }
+
+        static int CountWords(string input)
+        {
+            // Разделители слов - пробел, запятая, точка с запятой и т.д.
+            char[] delimiters = { ' ', ',', ';', '.', '!', '?' };
+
+            // Разбиваем предложение на слова, используя заданные разделители.
+            string[] words = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            // Возвращаем количество слов в предложении.
+            return words.Length;
+
+        }
+        static void Zad3()
+        {
+
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            bool isPalindrome = true;
+
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome)
+            {
+                Console.WriteLine("Введенная строка является палиндромом.");
+            }
+            else
+            {
+                Console.WriteLine("Введенная строка не является палиндромом.");
+            }
+        }
+        static void Zad4()
+        {
+
+            // Создаем генератор случайных чисел.
+            Random random = new Random();
+
+            // Создаем двумерный массив 5x5 и заполняем его случайными числами от -100 до 100.
+            int[,] array = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    array[i, j] = random.Next(-100, 101); // Генерируем числа от -100 до 100.
+                }
+            }
+
+            // Находим минимальный и максимальный элементы массива.
+            int minValue = array[0, 0];
+            int maxValue = array[0, 0];
+            int minRow = 0, minCol = 0, maxRow = 0, maxCol = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (array[i, j] < minValue)
+                    {
+                        minValue = array[i, j];
+                        minRow = i;
+                        minCol = j;
+                    }
+                    if (array[i, j] > maxValue)
+                    {
+                        maxValue = array[i, j];
+                        maxRow = i;
+                        maxCol = j;
+                    }
+                }
+            }
+
+            // Находим сумму элементов между минимальным и максимальным элементами.
+            int sum = 0;
+            int startRow = Math.Min(minRow, maxRow);
+            int endRow = Math.Max(minRow, maxRow);
+            int startCol = Math.Min(minCol, maxCol);
+            int endCol = Math.Max(minCol, maxCol);
+
+            for (int i = startRow + 1; i < endRow; i++)
+            {
+                for (int j = startCol + 1; j < endCol; j++)
+                {
+                    sum += array[i, j];
+                }
+            }
+
+            // Выводим результаты.
+            Console.WriteLine("Двумерный массив:");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Минимальный элемент: {minValue} (строка {minRow}, столбец {minCol})");
+            Console.WriteLine($"Максимальный элемент: {maxValue} (строка {maxRow}, столбец {maxCol})");
+            Console.WriteLine($"Сумма элементов между минимальным и максимальным элементами: {sum}");
+
+        }
+
+        static void Zad5()
+        {
+
+            Console.WriteLine("Введите текст:");
+            string input = Console.ReadLine();
+
+            char maxChar = '\0'; // Инициализируем символ как пустой символ.
+            int maxCount = 0;   // Инициализируем количество символов как 0.
+            char currentChar = '\0';
+            int currentCount = 0;
+
+            foreach (char c in input)
+            {
+                if (c == currentChar)
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    currentChar = c;
+                    currentCount = 1;
+                }
+
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                    maxChar = currentChar;
+                }
+            }
+
+            if (maxCount > 1)
+            {
+                Console.WriteLine($"Наибольшее количество идущих подряд одинаковых символов: '{maxChar}', количество: {maxCount}");
+            }
+            else
+            {
+                Console.WriteLine("В тексте нет повторяющихся символов подряд.");
+            }
+
+        }
+
+        static void Zad6()
+        {
+
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            int uniqueLetterCount = 0;
+            bool[] letterExists = new bool[32]; // Размер массива соответствует количеству русских букв.
+
+            foreach (char c in input)
+            {
+                if (char.IsUpper(c) && c >= 'А' && c <= 'Я')
+                {
+                    int index = c - 'А'; // Вычисляем индекс буквы в массиве.
+                    if (!letterExists[index])
+                    {
+                        letterExists[index] = true;
+                        uniqueLetterCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Количество различных букв: {uniqueLetterCount}");
+
+        }
+        static void Zad7()
+        {
+
+            Console.WriteLine("Введите строку длиной 20 символов:");
+            string input = Console.ReadLine();
+
+            if (input.Length != 20)
+            {
+                Console.WriteLine("Введенная строка должна быть длиной 20 символов.");
+            }
+            else
+            {
+                int digitCount = 0;
+
+                foreach (char c in input)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        digitCount++;
+                    }
+                }
+
+                Console.WriteLine($"Количество цифр в строке: {digitCount}");
+            }
+        }
+        static void Zad8()
+        {
+            Console.WriteLine("Введите текст из 20 символов:");
+            string input = Console.ReadLine();
+
+            char[] nameChars = "Ваше_имя".ToCharArray(); // Замените "Ваше_имя" на ваше имя
+
+            if (input.Length != 20)
+            {
+                Console.WriteLine("Нет имени");
+            }
+            else
+            {
+                bool isNamePossible = true;
+
+                for (int i = 0; i < nameChars.Length; i++)
+                {
+                    if (input[i] != nameChars[i])
+                    {
+                        isNamePossible = false;
+                        break;
+                    }
+                }
+
+                if (isNamePossible)
+                {
+                    Console.WriteLine("Поздравляем! Вы ввели ваше имя.");
+                }
+                else
+                {
+                    Console.WriteLine("Нет имени");
+                }
+            }
+
+        }
+        static void Zad9()
+        {
+
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            int count = 0;
+            bool isWord = false;
+            char firstChar = '\0';
+
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (!isWord)
+                    {
+                        isWord = true;
+                        firstChar = c;
+                    }
+                }
+                else if (isWord)
+                {
+                    if (c == firstChar)
+                    {
+                        count++;
+                    }
+                    isWord = false;
+                }
+            }
+
+            Console.WriteLine($"Количество слов с совпадающими первым и последним символами: {count}");
+        }
+        static void Zad10()
+        {
+            Console.WriteLine("Введите малую русскую букву:");
+            char lowercaseChar = Console.ReadKey().KeyChar;
+
+            if (lowercaseChar >= 'а' && lowercaseChar <= 'я')
+            {
+                char uppercaseChar = (char)(lowercaseChar - ('а' - 'А'));
+                Console.WriteLine($"\nСоответствующая большая буква: {uppercaseChar}");
+            }
+            else
+            {
+                Console.WriteLine("\nЭто не малая русская буква.");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+           // Zad1();
+           // Zad2();
+           // Zad3();
+           // Zad4();
+           // Zad5();
+           // Zad6();
+           // Zad7();
+           // Zad8();
+           // Zad9();
+           // Zad10();
+        }
+    }
+}
+
+    
+   
+
+
+
+     
+
+      
+
